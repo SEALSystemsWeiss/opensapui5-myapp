24debee7dab2f6c8d02b85e3787343d36b9966a8
SEALSystemsWeiss <christian.weiss@sealsystems.de>
2016-10-06 08:04:00 +0000

    initial from other project

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,60 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"opensap/myapp/model/formatter"
+], function (Controller, MessageToast, Filter, FilterOperator, formatter) {
+	"use strict";
+
+	return Controller.extend("opensap.myapp.controller.App", {
+
+		formatter : formatter,
+		
+		formatMapUrl: function(sStreet, sZip, sCity, sCountry) {
+			console.log("Data:");
+			console.log(sStreet);
+			console.log(sZip);
+			console.log(sCity);
+			console.log(sCountry);
+			return "https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=500x300&markers=" + jQuery.sap.encodeURL(sStreet + ", " + sZip +  " " + sCity + ", " + sCountry);
+		},
+		
+		onShowHello : function () {
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+		},
+
+		onFilterProducts : function (oEvent) {
+			// build filter array
+			var aFilter = [];
+			// fetch event parameter
+			var sQuery = oEvent.getParameter("query");
+			// retrieve list control
+			var oList = this.getView().byId("productsList");
+			// get binding for aggregation 'items'
+			var oBinding = oList.getBinding("items");
+
+			if (sQuery) {
+				aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+			}
+			// apply filter. an empty filter array will show all items
+			oBinding.filter(aFilter);
+		},
+
+		onItemSelected : function (oEvent) {
+			var oSelectedItem = oEvent.getParameter("listItem");
+			var oContext = oSelectedItem.getBindingContext();
+			var sPath = oContext.getPath();
+			var oProductDetailPanel = this.byId("productDetailsPanel");
+
+			oProductDetailPanel.bindElement({path : sPath });
+			this.byId("productDetailsPanel").setVisible(true); 
+		}
+	});
+});
